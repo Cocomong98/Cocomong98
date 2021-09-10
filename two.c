b8a10251aaaacd9ae943c005cdafe18c8490a957
Cocomong98 <90203932+Cocomong98@users.noreply.github.com>
2021-09-10 23:56:55 +0900

    Add files via upload

---
+++ two.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int a,b,sum;
+
+    printf("Enter 1st number :");
+    scanf("%d",a);
+    printf("Enter 2st number :");
+    scanf("%d",b);
+
+    sum=a+b;
+
+    printf("%d+%d=%d",a,b,sum);
+    
+    return 0;
+}
